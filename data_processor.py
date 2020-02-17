c7a2b45c6c8ab9f647567ff117643473ea852e49
zhousy1993 <zhousiyao1993@gmail.com>
2020-02-17 10:51:59 -0500

    add rubustness to the idx of loss and value

--- data_processor.py
+++ data_processor.py
@@ -9,16 +9,22 @@ x_axis = []
 y_axis = []
 for line in f:
     if re.search('loss', line):
-        res.append(line.split()[6:10])
-print(float(res[0][3]))
+        # print(line.split())
+        idx_iter = line.split().index('iter:')
+        # print(idx_iter)
+        res.append(line.split()[idx_iter:idx_iter+4])
+# print(res)
+
+
+
 for line in range(len(res)):
     x_axis.append(float(res[line][1]))
     y_axis.append(float(res[line][3]))
 
-print(x_axis)
-print(len(x_axis))
-print(y_axis)
-print(len(y_axis))
+# print(x_axis)
+# print(len(x_axis))
+# print(y_axis)
+# print(len(y_axis))
 
 plt.plot(x_axis, y_axis)
 plt.show()
